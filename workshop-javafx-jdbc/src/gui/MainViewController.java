7d1f5d81937995e6595d9f254ad97ee5439fc012
Dyego Henrique Costa Braga <dyego.hcb@gmail.com>
2023-12-11 15:11:32 -0300

    Department View Desing

--- workshop-javafx-jdbc/src/gui/MainViewController.java
+++ workshop-javafx-jdbc/src/gui/MainViewController.java
@@ -34,8 +34,7 @@ public class MainViewController implements Initializable {
 
 	@FXML
 	public void onMenuItemDepartmentAction() {
-		System.out.println("onMenuItemDepartmentAction");
-
+		loadView("/gui/DepartmentList.fxml");
 	}
 
 	@FXML
